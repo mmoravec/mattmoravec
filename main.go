32f563db3e965bb657900f0d76c00f53914f9383
Matt Moravec <mmoravec09@gmail.com>
2016-09-18 16:12:20 -0700

    set port

--- main.go
+++ main.go
@@ -4,14 +4,21 @@ import (
     "net/http"
     "html/template"
     "fmt"
+    "os"
 )
 
 func main() {
-    http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-      fmt.Println(r.URL)
-      tmpl :=  template.Must(template.ParseFiles("index.html"))
-      tmpl.Execute(w, nil)
-    })
-    http.ListenAndServe(":8000", nil)
+  port := os.Getenv("PORT")
+
+  if port == "" {
+    log.Fatal("$PORT must be set")
+  }
+
+  http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
+  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    fmt.Println(r.URL)
+    tmpl :=  template.Must(template.ParseFiles("index.html"))
+    tmpl.Execute(w, nil)
+  })
+  http.ListenAndServe(":" + port, nil)
 }
